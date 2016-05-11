c7f25f113270ff8280882a3b5eae6b4c91a8b2db
Tim Hornick (AKA Xydium) <prodriver54@gmail.com>
2016-05-11 11:51:58 -0400

    More Shader Updates

---
+++ Tenebrae Incarnati/res/shaders/basic-shader.vs
@@ -0,0 +1,8 @@
+varying vec2 texCoord;
+
+void main()
+{
+	texCoord = vec2(gl_MultiTexCoord0.x, gl_MultiTexCoord0.y);
+	
+	gl_Position = gl_Vertex;
+}
---
+++ Tenebrae Incarnati/res/shaders/distort-shader.fs
@@ -0,0 +1,57 @@
+
+uniform sampler2D currentTexture;
+//uniform sampler2D distortionMapTexture;
+
+uniform float time;
+
+uniform float frequency;
+uniform float amplitude;
+
+//uniform float distortionFactor;
+//uniform float riseFactor;
+
+varying vec2 texCoord;
+
+float clamp(float a, float min, float max)
+{
+	if (a < min)
+		return min;
+	
+	if (a > max)
+		return max;
+	
+	return a;
+}
+
+void main()
+{
+	vec2 aux = vec2(1.0 - texCoord.y, texCoord.x);
+	aux.x = clamp(sin(30.0 * aux.y + frequency * time) * amplitude + aux.x, 0.0, 1.0);
+	aux.y = clamp(sin(30.0 * aux.x + frequency * time) * amplitude + aux.y, 0.0, 1.0);
+	
+	gl_FragColor = texture2D(currentTexture, aux);
+}
+
+/*void main()
+{
+	vec2 distortionMapCoord = texCoord.st;
+	
+	distortionMapCoord.t -= time * riseFactor;
+	
+	vec4 distortionMapValue = texture2D(distortionMapTexture, distortionMapCoord);
+	
+	vec2 distortionPositionOffset = distortionMapValue.xy;
+	distortionPositionOffset -= vec2(0.5f, 0.5f);
+	distortionPositionOffset *= 2.f;
+	
+	distortionPositionOffset *= distortionFactor;
+	
+	vec2 distortionUnused = distortionMapValue.zw;
+	
+	distortionPositionOffset *= (1.f - texCoord.t);
+	
+	vec2 distortedTexCoord = texCoord.st + distortionPositionOffset;
+	
+	//gl_FragColor = gl_Color * texture2D(currentTexture, distortedTexCoord);
+	gl_FragColor = gl_Color * texture2D(currentTexture, distortedTexCoord);
+}*/
