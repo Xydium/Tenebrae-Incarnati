e377c01164842cd3c761a6a6be3c2a942c46e9ba
Tim Hornick (AKA Xydium) <prodriver54@gmail.com>
2016-05-11 15:45:50 -0400

    Splash Screen/Main Menu partial

--- src/fizzion/tenebrae/scene/DebugSplash.java
+++ src/fizzion/tenebrae/scene/DebugSplash.java
@@ -1,8 +1,10 @@
 package fizzion.tenebrae.scene;
 
 import engine.components.RectRenderer;
+import engine.components.RectRenderer.UniformConfig;
 import engine.core.Scene;
 import engine.math.Vector2;
+import engine.rendering.Color;
 import engine.rendering.Shader;
 import engine.rendering.Texture;
 import engine.utility.Time;
@@ -12,32 +14,58 @@ public class DebugSplash extends Scene
 {
 	
 	private Texture splash;
-	private Shader splashShader;
+	private Shader texShader, colorShader;
 	
 	private double startTime;
 	
 	public void activate()
 	{
 		splash = new Texture("backgrounds/prototype_build.png");
-		splashShader = new Shader("basic-shader");
-		RectRenderer r = new RectRenderer(Util.pixelDToGL(new Vector2(1024f, 576f)), splash);
-		r.setShader(splashShader);
-		getRootObject().addComponent(r);
+		texShader = new Shader("basic-shader");
+		colorShader = new Shader("color-shader");
+		final RectRenderer splashRect = new RectRenderer(Util.pixelDToGL(new Vector2(1024f, 576f)), splash);
+		final RectRenderer veilRect = new RectRenderer(Util.pixelDToGL(new Vector2(1024f, 576f)), splash);
+		splashRect.setShader(texShader);
+		veilRect.setShader(colorShader);
+		
+		veilRect.setUniformConfig(new UniformConfig() 
+		{
+			public void setUniforms() 
+			{
+				veilRect.getShader().setUniform("color", new Color(0.0f, 0.0f, 0.0f, calculateAlpha()));
+			}
+		});
+		
+		getRootObject().addAllComponents(splashRect, veilRect);
 		startTime = Time.getTime();
 	}
 	
 	public void update()
 	{
-		if(Time.getTime() - startTime > 3)
+		if(Time.getTime() - startTime > 10)
 		{
-			getApplication().getGame().setScene(new Scene());
+			getApplication().getGame().setScene(new MainMenu());
 		}
 	}
 	
 	public void deactivate()
 	{
 		splash = null;
-		splashShader = null;
+		texShader = colorShader = null;
+	}
+	
+	private float calculateAlpha() 
+	{
+		float timeDif = (float) (Time.getTime() - startTime);
+		if(timeDif <= 3) 
+		{
+			return 1.0f - (timeDif / 3);
+		} 
+		else if(timeDif >= 7) 
+		{
+			return (timeDif - 7) / 3;
+		}
+		return 0.0f;
 	}
 	
 }
---
+++ src/fizzion/tenebrae/scene/MainMenu.java
@@ -0,0 +1,107 @@
+package fizzion.tenebrae.scene;
+
+import engine.components.RectRenderer;
+import engine.components.RectRenderer.UniformConfig;
+import engine.core.Input;
+import engine.core.Scene;
+import engine.math.Vector2;
+import engine.rendering.Color;
+import engine.rendering.Shader;
+import engine.rendering.Texture;
+import engine.utility.Log;
+import engine.utility.Util;
+
+public class MainMenu extends Scene 
+{
+
+	private Texture atmo, platform, obetext, skull, title;
+	private Shader texShader, disShader, colorShader;
+	
+	private float i;
+	
+	public void activate()
+	{
+		atmo = new Texture("backgrounds/menu_background_atmo.png");
+		platform = new Texture("backgrounds/menu_background_fore.png");
+		obetext = new Texture("backgrounds/menu_background_obetext.png");
+		skull = new Texture("backgrounds/menu_background_skull.png");
+		title = new Texture("backgrounds/menu_background_title.png");
+		
+		texShader = new Shader("basic-shader");
+		disShader = new Shader("distort-shader");
+		colorShader = new Shader("color-shader");
+		colorShader.setUniform("color", new Color(0f, 0f, 0f, 0f));
+		
+		final RectRenderer atmoRect = new RectRenderer(Util.pixelDToGL(new Vector2(1024f, 576f)), atmo);
+		final RectRenderer platformRect = new RectRenderer(Util.pixelDToGL(new Vector2(1024f, 576f)), platform);
+		final RectRenderer obetextRect = new RectRenderer(Util.pixelDToGL(new Vector2(1024f, 576f)), obetext);
+		final RectRenderer skullRect = new RectRenderer(Util.pixelDToGL(new Vector2(1024f, 576f)), skull);
+		final RectRenderer titleRect = new RectRenderer(Util.pixelDToGL(new Vector2(1024f, 576f)), title);
+		
+		atmoRect.setShader(disShader);
+		platformRect.setShader(texShader);
+		obetextRect.setShader(disShader);
+		obetextRect.setTag("obetext");
+		skullRect.setShader(texShader);
+		titleRect.setShader(disShader);
+		
+		atmoRect.setUniformConfig(new UniformConfig() 
+		{
+			public void setUniforms()
+			{
+				atmoRect.getShader().setUniform("time", i);
+				atmoRect.getShader().setUniform("frequency", 5.0f);
+				atmoRect.getShader().setUniform("amplitude", 0.03f);
+			}
+		});
+		
+		obetextRect.setUniformConfig(new UniformConfig() 
+		{
+			public void setUniforms()
+			{
+				if(obetextRect.getShader().getFileName().equals("distort-shader")) {
+					obetextRect.getShader().setUniform("time", -i / 2);
+					obetextRect.getShader().setUniform("frequency", -10.0f);
+					obetextRect.getShader().setUniform("amplitude", -0.0007f);
+				}
+			}
+		});
+		
+		titleRect.setUniformConfig(new UniformConfig() 
+		{
+			public void setUniforms()
+			{
+				titleRect.getShader().setUniform("time", i);
+				titleRect.getShader().setUniform("frequency", 1.0f);
+				titleRect.getShader().setUniform("amplitude", 0.01f);
+			}
+		});
+		
+		getRootObject().addAllComponents(atmoRect, platformRect, obetextRect, skullRect, titleRect);
+	}
+
+	public void input()
+	{
+		Vector2 mp = Input.getMousePosition();
+		if(mp.getX() > 360 && mp.getY() < 660 && mp.getY() > 150 && mp.getY() < 260) 
+		{
+			((RectRenderer) getRootObject().getComponentWithTag("obetext")).setShader(disShader);
+		}
+		else
+		{
+			((RectRenderer) getRootObject().getComponentWithTag("obetext")).setShader(colorShader);
+		}
+	}
+	
+	public void update()
+	{
+		i += 0.02;
+	}
+	
+	public void deactivate()
+	{
+		atmo = platform = obetext = skull = title = null;
+		texShader = disShader = colorShader = null;
+	}
+	
+}
