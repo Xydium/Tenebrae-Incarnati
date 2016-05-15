f0de425db30f3e8e3db71d6ee20834a151c584e7
Tim Hornick (AKA Xydium) <prodriver54@gmail.com>
2016-05-15 14:28:23 -0400

    Uses log window

--- src/fizzion/tenebrae/launch/Main.java
+++ src/fizzion/tenebrae/launch/Main.java
@@ -20,8 +20,8 @@ public class Main extends Game
 	public static void main(String[] args)
 	{
 		flags = new WindowFlags("Tenebrae Incarnati", 512 * 2, 288 * 2);
-		flags.setLogLevel(LogLevel.INTERNAL);
-		flags.setConsoleEnabled(true);
+		Log.setLogLevel(LogLevel.INTERNAL);
+		Log.setWindowEnabled(true);
 		Application app = new Application(new Main(), 60.0, flags);
 		app.start();
 	}
