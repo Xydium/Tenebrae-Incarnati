049378ecb9e5eb34b878cea3df84a92c80f61603
Chris Jerrett <cjerrett18@stjohnsprep.org>
2016-05-21 12:01:29 -0400

    new room

--- src/fizzion/tenebrae/scene/DebugSplash.java
+++ src/fizzion/tenebrae/scene/DebugSplash.java
@@ -13,24 +13,24 @@ import engine.utility.Time;
 import fizzion.tenebrae.launch.TenebraeIncarnati;
 
 /**
- * 
+ *
  * @author Tim Hornick
  *
  */
-public class DebugSplash extends Scene 
+public class DebugSplash extends Scene
 {
-	
+
 	private double startTime;
-	
+
 	public void load()
 	{
 		GameObject splash = new Splash();
 		GameObject veil = new Veil();
-		
+
 		getRootObject().addAllChildren(splash, veil);
 		startTime = Time.getTime();
 	}
-	
+
 	public void input()
 	{
 		if (Input.getMouseDown(Input.MOUSE_LEFT) || Input.getKeyDown(Input.KEY_ESCAPE))
@@ -39,16 +39,16 @@ public class DebugSplash extends Scene
 			ti.setScene(ti.getScene("MainMenu"));
 		}
 	}
-	
+
 	public void update()
 	{
-		if(Time.getTime() - startTime > 10)
+		if(Time.getTime() - startTime > 5)
 		{
 			TenebraeIncarnati ti = (TenebraeIncarnati)getApplication().getGame();
 			ti.setScene(ti.getScene("MainMenu"));
 		}
 	}
-	
+
 	private class Splash extends GameObject
 	{
 		public Splash()
@@ -59,9 +59,9 @@ public class DebugSplash extends Scene
 			splashRect.setAllowLighting(false);
 			splashRect.setShader(textureShader);
 			addComponent(splashRect);
-		}	
+		}
 	}
-	
+
 	private class Veil extends GameObject
 	{
 		public Veil()
@@ -70,29 +70,29 @@ public class DebugSplash extends Scene
 			RectRenderer veilRect = new RectRenderer(new Vector2i(1024f, 576f), null);
 			veilRect.setAllowLighting(false);
 			veilRect.setShader(colorShader);
-			veilRect.setUniformConfig(new UniformConfig() 
+			veilRect.setUniformConfig(new UniformConfig()
 			{
-				public void setUniforms(Shader s) 
+				public void setUniforms(Shader s)
 				{
 					s.setUniform("color", new Color(0.0f, 0.0f, 0.0f, calculateAlpha()));
 				}
 			});
 			addComponent(veilRect);
 		}
-		
-		private float calculateAlpha() 
+
+		private float calculateAlpha()
 		{
 			float timeDif = (float) (Time.getTime() - startTime);
-			if(timeDif <= 3) 
+			if(timeDif <= 3)
 			{
 				return 1.0f - (timeDif / 3);
-			} 
-			else if(timeDif >= 7) 
+			}
+			else if(timeDif >= 7)
 			{
 				return (timeDif - 7) / 3;
 			}
 			return 0.0f;
 		}
 	}
-	
+
 }
